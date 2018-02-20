0417b4abcae1bb9e07a5cdfd88fbc7f39fe202c0
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-02-20 13:47:41 +0100

    day 3 en 4 complete

--- advent.js
+++ advent.js
@@ -1,110 +1,43 @@
-calcAll();
+const fs = require("fs");
 
-function gridNul() {
-  var grid = [];
-  grid[0] = [0, 0, 0, 1];
-  var gridNamen = ["index van tile ", "index van circle ", "index van tile in circle ", "value van tile "];
-  for (var i = 0; i < gridNamen.length; i++) {
-    //console.log(gridNamen[i] + grid[0][i]);
+var input = fs.readFile("input3.txt", "utf8", (err, data) => {
+  if (err) {
+    return console.error(err);
   }
-  return [grid, gridNamen] ;
-}
-
-function tilesInCircle(n) {
-  if (n == 0) {
-    return 1;
-  } else {
-    var circle = (n * 2) + 1;
-    var squares = ((circle - 1) * 4);
-    //console.log("loop squares: " + squares);
-    return squares;
-  }
-}
-
-function calcNext(grid) {
-  var grid = grid
-  var lastGrid = [];
-  lastGrid = grid[grid.length - 1];
-  //console.log("lastGrid: " + lastGrid);
-  var newestGrid = [0, 0, 0, 0];
-  newestGrid[0] = lastGrid[0] + 1;
-  if (lastGrid[2] >= (tilesInCircle(lastGrid[1]) - 1)) {
-    newestGrid[1] = lastGrid[1] + 1;
-    newestGrid[2] = 0;
-  } else {
-    newestGrid[1] = lastGrid[1];
-    newestGrid[2] = lastGrid[2] + 1;
-  }
-  var newestGridValue = 0;
-  if (lastGrid[1] == (newestGrid[1] - 1)) {
-    newestGridValue = lastGrid[3];
-    if (lastGrid[1] >= 1) {
-      var indexFirstCircle = lastGrid[0] - tilesInCircle(lastGrid[1]) + 1;
-      newestGridValue += grid[indexFirstCircle][3];
+  else {
+    x = data;
+    doAll(x);
+    return x;
     }
-  } else if (newestGrid[2] == (tilesInCircle(newestGrid[1]) - 1)) {
-    newestGridValue = lastGrid[3];
-    var indexLastPreCircle = lastGrid[0] - tilesInCircle(lastGrid[1]) + 1;
-    newestGridValue += grid[indexLastPreCircle][3];
-    var indexFirstCircle = lastGrid[0] - tilesInCircle(lastGrid[1]) + 2;
-    newestGridValue += grid[indexFirstCircle][3];
-  } else if (true) {
-    newestGridValue = lastGrid[3];
-    var distToCorner = 0;
-    var circleQuartersDist = tilesInCircle(newestGrid[1])/4;
-    var circleQuarters = [0, 0, 0, 0];
-    for (var i = 0; i < 4; i++) {
-      circleQuarters[i] = i * circleQuartersDist;
+});
+
+var x;
+
+function doAll(x) {
+  var valid = 512;
+  x = x.split("\n");
+  x.pop();
+  for (var i = 0; i < x.length; i++) {
+    var testFail = 0
+    x[i] = x[i].split(" ");
+    var testCases = [];
+    for (var j = 0; j < x[i].length; j++) {
+      testCases[j] = x[i][j].split("");
+      testCases[j] = testCases[j].sort().join("");
     }
-    if (newestGrid[1] == 1) {
-      var nothing;
-    } else if (newestGrid[2] < (circleQuarters[1]) && newestGrid[2] > 0) {
-      newestGridValue = lastGrid[3];
-      var tilesInLastGrid = tilesInCircle((newestGrid[1] - 1));
-      var indexSimLastCircle = [0, 0, 0];
-      indexSimLastCircle[0] = newestGrid[0] - (newestGrid[2] + 1) - (tilesInLastGrid) + (newestGrid[2]);
-      indexSimLastCircle[1] = newestGrid[0] - (newestGrid[2] + 1) - (tilesInLastGrid) + (newestGrid[2] - 1);
-      console.log("indexSimLastCircle: " + indexSimLastCircle);
-      if (grid[indexSimLastCircle[1]][1] != grid[indexSimLastCircle[0]][1]) {
-        indexSimLastCircle[1] = newestGrid[0] - 2;
-      }
-      indexSimLastCircle[2] = newestGrid[0] - (newestGrid[2] + 1) - (tilesInLastGrid) + (newestGrid[2] + 1);
-      if (circleQuarters[1] = (newestGrid[2] + 1)) {
-        indexSimLastCircle.pop();
-        console.log("dit is de voor laatste voor hoek");
-      }
-      if (newestGrid[2] == circleQuarters[1]) {
-        console.log("is hoek");
-        indexSimLastCircle[0] = indexSimLastCircle[1];
-        indexSimLastCircle.pop();
-        indexSimLastCircle.pop();
-        console.log(indexSimLastCircle);
-      }
-      console.log("indexSimLastCircle: " + indexSimLastCircle);
-      for (var i = 0; i < indexSimLastCircle.length; i++) {
-        newestGridValue += grid[indexSimLastCircle[i]][3];
-        console.log("de toevoeging van grid: " + grid[indexSimLastCircle[i]]);
+    for (var k = 0; k < x[i].length; k++) {
+      for (var l = 0; l < x[i].length; l++) {
+        var testValue = x[i][k].split("");
+        testValue = testValue.sort().join("");
+        if (testValue == testCases[l] && k != l) {
+          testFail = 1;
+          console.log(x[i]);
+        }
       }
     }
-  }
-  newestGrid[3] = newestGridValue;
-  //console.log("newestGrid: " + newestGrid);
-  grid[grid.length] = newestGrid;
-  //console.log("grid return whole is: " + grid);
-  return grid;
-}
-
-function calcAll() {
-  var gridIni = gridNul();
-  var grid = gridIni[0];
-  var gridNamen = gridIni[1];
-  var puzzleInput = 20;//312051;
-  var gridValue = 0;
-  while (gridValue < puzzleInput) {
-    grid = calcNext(grid);
-    console.log("grid return whole is: ");
-    console.log(grid);
-    gridValue = grid[grid.length - 1][3];
-    console.log("gridValue in loop is: " + gridValue);
+    if (testFail == 1) {
+      valid --;
+      console.log(valid);
+    }
   }
 }
--- advent2.js
+++ advent2.js
@@ -129,16 +129,46 @@ function calcNext(grid) {
       console.log("midden links");
     } else if (newestGrid[2] == (gridCorners[2] - 2)) {
       newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 3)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 4)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 4)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
       console.log("een na laatste links");
     } else if (newestGrid[2] == (gridCorners[2] - 1)) {
       newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 4)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
       console.log("laastste links");
     }
   } else if (newestGrid[2] < gridCorners[3]) {
-    newestGrid[3] = 4;
+    if (newestGrid[1] == 1) {
+      if (newestGrid[2] == 6) {
+        newestGrid[3] = 23;
+      } else if (newestGrid[2] == 7) {
+        newestGrid[3] = 25;
+      }
+      console.log("eerste ring onder");
+    } else if (newestGrid[2] == (gridCorners[2])) {
+      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
+      newestGrid[3] += grid[(newestGrid[0] - 2)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 6)][3];
+      console.log("eerste onder");
+    } else if (newestGrid[2] < (gridCorners[3] - 2)) {
+      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 6)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 7)][3];
+      console.log("midden onder");
+    } else if (newestGrid[2] == (gridCorners[3] - 2)) {
+      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 6)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 7)][3];
+      console.log("een na laatste onder");
+    } else if (newestGrid[2] == (gridCorners[3] - 1)) {
+      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 6)][3];
+      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 7)][3];
+      console.log("laastste onder");
+    }
   }
   grid[grid.length] = newestGrid;
   //console.log("grid return whole is: " + grid);
@@ -149,10 +179,10 @@ function calcAll() {
   var gridIni = gridNul();
   var grid = gridIni[0];
   var gridNamen = gridIni[1];
-  var puzzleInput = 200;//312051;
+  var puzzleInput = 312051;
   var gridValue = 0;
   var counter = 0
-  while (gridValue < puzzleInput && counter < 40) {
+  while (gridValue < puzzleInput && counter < 400) {
     counter ++;
     grid = calcNext(grid);
     console.log("grid return whole is: ");
