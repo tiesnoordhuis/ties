91409e06d39a743830aaf443b47976c5646fea2f
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-03-01 11:28:17 +0100

    day 7 part 2

--- advent.js
+++ advent.js
@@ -29,6 +29,7 @@ function doAll(x) {
   }
   var listPosRow = [];
   var listPos = [];
+  var totalRedirects = 0;
   for (var i = 0; i < list.length; i++) {
   	listPosRow[0] = i;
     listPosRow[1] = list[i].length;
@@ -38,12 +39,33 @@ function doAll(x) {
       for (var j = 0; j < list.length; j++) {
         for (var k = 1; k < list[j].length; k++) {
           if (list[i][0] == list[j][k] && j != i) {
-            listPosRow[2] = [1000, j , k];
+            listPosRow[2] = [0, j , k];
+            totalRedirects ++;
           }
         }
       }
     }
     listPos[i] = listPosRow.map(e => e);
     console.log(listPos[i]);
+  }
+  console.log(list[1016]);
+  while (totalRedirects > 0) {
+      for (var i = 0; i < listPos.length; i++) {
+      if (listPos[i][1] > 1) {
+        var listPosRefTile = listPos[i][2][1];
+        console.log("listPosRefTile " + listPosRefTile);
+        if (listPos[listPosRefTile][2] != 0) {
+          listPos[i][2][0] ++;
+          listPos[i][2][1] = listPos[listPosRefTile][2][1];
+          console.log("listpos update: " + listPos[i]);
+        } else {
+          var listPosEndTile = listPos[i][2][0];
+          listPos[i][2] = (listPosEndTile + 1);
+          console.log("listpos update endpoint: " + listPos[i]);
+          totalRedirects --;
+        }
+      }
+    }
+    console.log("total redirects: " + totalRedirects);
   }
 }
