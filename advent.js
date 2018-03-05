cd56c3a73eb4e626c4ec506f018d095c66ef5302
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-03-06 00:24:40 +0100

    sum weights in list

--- advent.js
+++ advent.js
@@ -14,58 +14,98 @@ var input = fs.readFile("input5.txt", "utf8", (err, data) => {
 var x;
 
 function doAll(x) {
+  var list2 = [];
   var list = x.split("\n");
   list.pop();
   for (var i = 0; i < list.length; i++) {
     var newListEntry = list[i].split(" -> ");
-    newListEntry[0] = newListEntry[0].split(" ")[0];
+    //newListEntry[0] = newListEntry[0].split(" ")[0];
     list[i] = newListEntry;
     if (list[i].length > 1) {
       var newListEntrySecond = list[i][1].split(", ");
-      for (var j = 0; j < newListEntrySecond.length; j++) {
-        list[i][(j + 1)] = newListEntrySecond[j];
+      list[i][1] = newListEntrySecond;
+    } else {
+      list[i][1] = [""];
+    }
+    var newListEntryWeight = list[i][0].split(" (");
+    list[i][0] = newListEntryWeight[0];
+    list[i][2] = Number(newListEntryWeight[1].split(")")[0]);
+    list[i][3] = 0;
+    list2[i] = [i, list[i][1].slice()];
+    console.log(list[i]);
+  }
+  var listWeights = [];
+  for (var i = 0; i < list.length; i++) {
+    listWeights[i] = [];
+    for (var j = 0; j < list[i][1].length; j++) {
+      for (var k = 0; k < list.length; k++) {
+        if (list[i][1][j] == list[k][0] && k != i) {
+          listWeights[i][j] = list[k][2];
+        }
       }
     }
   }
-  var listPosRow = [];
-  var listPos = [];
-  var totalRedirects = 0;
-  for (var i = 0; i < list.length; i++) {
-  	listPosRow[0] = i;
-    listPosRow[1] = list[i].length;
-    if (listPosRow[1] == 1) {
-      listPosRow[2] = 0;
-    } else if (listPosRow[1] > 1) {
-      for (var j = 0; j < list.length; j++) {
-        for (var k = 1; k < list[j].length; k++) {
-          if (list[i][0] == list[j][k] && j != i) {
-            listPosRow[2] = [0, j , k];
-            totalRedirects ++;
+  //console.log(listWeights);
+  var falseWeights = [];
+  for (var i = 0; i < listWeights.length; i++) {
+    var checked = 0;
+    var referenceValue = listWeights[i][0];
+    for (var j = 1; j < listWeights[i].length; j++) {
+      if (listWeights[i][j] == referenceValue) {
+
+      } else if (listWeights[i][j] > referenceValue) {
+        checked = 1;
+      } else if (listWeights[i][j] < referenceValue) {
+        checked = 1;
+      }
+    }
+    if (checked != 0) {
+      //console.log(listWeights[i]);
+      falseWeights.push([i, listWeights[i]])
+    }
+  }
+  //console.log(falseWeights);
+  var whileCheck = 0
+  console.log(checkDone(list));
+  while (whileCheck == 0) {
+    if (checkDone(list)) {
+      whileCheck = 1;
+    }
+    for (var i = 0; i < list.length; i++) {
+      if (list[i][1] == "") {
+        list[i][3] = 1;
+        //console.log(list[i]);
+      } else {
+        //console.log(list[i]);
+        for (var k = 0; k < list.length; k++) {
+          for (var j = 0; j < list[i][1].length; j++) {
+            if (list[k][0] == list[i][1][j] && list[k][3] == 1) {
+              //console.log(list[i]);
+              list[i][1].splice(j, 1);
+              list[i][2] += list[k][2];
+              //console.log(list[i]);
+            }
           }
         }
       }
+      //console.log(list[i]);
     }
-    listPos[i] = listPosRow.map(e => e);
-    console.log(listPos[i]);
   }
-  console.log(list[1016]);
-  while (totalRedirects > 0) {
-      for (var i = 0; i < listPos.length; i++) {
-      if (listPos[i][1] > 1) {
-        var listPosRefTile = listPos[i][2][1];
-        console.log("listPosRefTile " + listPosRefTile);
-        if (listPos[listPosRefTile][2] != 0) {
-          listPos[i][2][0] ++;
-          listPos[i][2][1] = listPos[listPosRefTile][2][1];
-          console.log("listpos update: " + listPos[i]);
-        } else {
-          var listPosEndTile = listPos[i][2][0];
-          listPos[i][2] = (listPosEndTile + 1);
-          console.log("listpos update endpoint: " + listPos[i]);
-          totalRedirects --;
-        }
+  for (var i = 0; i < list.length; i++) {
+    console.log(list[i]);
+    console.log(list2[i][1]);
+  }
+}
+
+function checkDone(list) {
+  var checkCounter = 0;
+  for (var i = 0; i < list.length; i++) {
+    if (list[i][3] == 0) {
+      checkCounter ++;
+      if (checkCounter > 2) {
+        return false;
       }
     }
-    console.log("total redirects: " + totalRedirects);
   }
+  return true;
 }
