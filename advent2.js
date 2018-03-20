1f0d5da2d1f85823a7c1c29ef03884cfed40ed08
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-03-20 12:06:13 +0100

    verdere day7 puzzle

--- advent2.js
+++ advent2.js
@@ -1,193 +1,91 @@
-calcAll();
+const fs = require("fs");
 
-function gridNul() {
-  var grid = [];
-  grid[0] = [0, 0, 0, 1];
-  var gridNamen = ["index van tile ", "index van circle ", "index van tile in circle ", "value van tile "];
-  for (var i = 0; i < gridNamen.length; i++) {
-    //console.log(gridNamen[i] + grid[0][i]);
+var input = fs.readFile("input5.txt", "utf8", (err, data) => {
+  if (err) {
+    return console.error(err);
   }
-  return [grid, gridNamen] ;
+  else {
+    x = data;
+    doAll(x);
+    return x;
+    }
+});
+
+var x;
+
+function doAll(x) {
+  var list = x.split("\n");
+  list.pop();
+  for (var i = 0; i < list.length; i++) {
+    var newListEntry = list[i].split(" -> ");
+    //newListEntry[0] = newListEntry[0].split(" ")[0];
+    list[i] = newListEntry;
+    if (list[i].length > 1) {
+      var newListEntrySecond = list[i][1].split(", ");
+      list[i][1] = newListEntrySecond;
+    } else {
+      list[i][1] = [""];
+    }
+    var newListEntryWeight = list[i][0].split(" (");
+    list[i][0] = newListEntryWeight[0];
+    list[i][2] = Number(newListEntryWeight[1].split(")")[0]);
+    list[i][3] = 0;
+    list[i][4] = Number(list[i][2]);
+    list[i][5] = [];
+  }
+  for (var i = 0; i < list.length; i++) {
+    if (list[i][1] != "") {
+      list[i][3] = 1;
+    }
+  }
+  for (var i = 0; i < list.length; i++) {
+    //console.log(list[i]);
+  }
+  var refCounter = 0;
+  var answerArray = [];
+  while (refCounter < 3) {
+    for (var i = 0; i < list.length; i++) {
+      if (list[i][3] == refCounter) {
+        for (var k = 0; k < list[i][1].length; k++) {
+          answerArray[0] = lookupWeight(list[i][1][k], refCounter, list);
+          var alreadySet = 0;
+          for (var j = 0; j < list[i][5].length; j++) {
+            if (list[i][5][j][1] == list[i][1][k]) {
+              alreadySet = 1;
+              console.log("already set");
+            }
+          }
+          if (answerArray[0][1] == 1 && alreadySet == 0) {
+            list[i][5][(list[i][5].length)] = [answerArray[0][0], answerArray[0][2]];
+          }
+        }
+        //console.log(list[i][5].length);
+        //console.log(list[i][1].length);
+        if (list[i][5].length == list[i][1].length || list[i][3] == 0) {
+          console.log("hetzelfde");
+          //list[i][3] = refCounter; //niet nodig
+          console.log(list[i][3]);
+        } else if (list[i][5].length != list[i][1].length) {
+          list[i][3] = (refCounter + 1);
+          console.log("niet hetzelfde");
+        }
+        console.log(list[i]);
+      }
+    }
+    //console.log(refCounter);
+    refCounter ++;
+  }
 }
 
-function tilesInCircle(n) {
-  if (n == 0) {
-    return 1;
-  } else {
-    var circle = (n * 2) + 1;
-    var squares = ((circle - 1) * 4);
-    //console.log("loop squares: " + squares);
-    return squares;
-  }
-}
-
-function calcNext(grid) {
-  var grid = grid
-  var lastGrid = [];
-  lastGrid = grid[grid.length - 1];
-  //console.log("lastGrid: " + lastGrid);
-  var newestGrid = [0, 0, 0, 0];
-  newestGrid[0] = lastGrid[0] + 1;
-  if (lastGrid[2] >= (tilesInCircle(lastGrid[1]) - 1)) {
-    newestGrid[1] = lastGrid[1] + 1;
-    newestGrid[2] = 0;
-  } else {
-    newestGrid[1] = lastGrid[1];
-    newestGrid[2] = lastGrid[2] + 1;
-  }
-  var gridCorners = [0, 0, 0, 0];
-  var tilesInThisCircle = tilesInCircle(newestGrid[1]);
-  for (var i = 0; i < gridCorners.length; i++) {
-    gridCorners[i] = ((tilesInThisCircle / 4) * (i + 1));
-  }
-  console.log("gridCorners:  " + gridCorners);
-  if (newestGrid[0] == 1) {
-    newestGrid[3] = 1;
-  } else if (newestGrid[2] < gridCorners[0]) {
-    if (newestGrid[1] == 1) {
-      if (newestGrid[2] == 0) {
-        newestGrid[3] = 1;
-      } else if (newestGrid[2] == 1) {
-        newestGrid[3] = 2;
+function lookupWeight(name, cycle, checkList) {
+  var weight = 0;
+  for (var i = 0; i < checkList.length; i++) {
+    if (checkList[i][0] == name) {
+      if (checkList[i][3] < cycle) {
+        weight = checkList[i][2];
+        return [weight, 1, name];
       }
-      console.log("eerste ring rechts");
-    } else if (newestGrid[2] == 0) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[((newestGrid[0]) - tilesInCircle((newestGrid[1] - 1)))][3];
-      console.log("begin circle");
-    } else if (newestGrid[2] == 1) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - 2)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2])][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 1)][3];
-      console.log("eerste rechts");
-    } else if (newestGrid[2] < (gridCorners[0] - 2)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2])][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 2)][3];
-      console.log("midden rechts");
-    } else if (newestGrid[2] == (gridCorners[0] - 2)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 2)][3];
-      console.log("een na laatste rechts");
-    } else if (newestGrid[2] == (gridCorners[0] - 1)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 2)][3];
-      console.log("laastste rechts");
-    }
-  } else if (newestGrid[2] < gridCorners[1]) {
-    if (newestGrid[1] == 1) {
-      if (newestGrid[2] == 2) {
-        newestGrid[3] = 4;
-      } else if (newestGrid[2] == 3) {
-        newestGrid[3] = 5;
-      }
-      console.log("eerste ring boven");
-    } else if (newestGrid[2] == (gridCorners[0])) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - 2)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 2)][3];
-      console.log("eerste boven");
-    } else if (newestGrid[2] < (gridCorners[1] - 2)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 2)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 3)][3];
-      console.log("midden boven");
-    } else if (newestGrid[2] == (gridCorners[1] - 2)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 3)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 4)][3];
-      console.log("een na laatste boven");
-    } else if (newestGrid[2] == (gridCorners[1] - 1)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + newestGrid[2] - 4)][3];
-      console.log("laastste boven");
-    }
-  } else if (newestGrid[2] < gridCorners[2]) {
-    if (newestGrid[1] == 1) {
-      if (newestGrid[2] == 4) {
-        newestGrid[3] = 10;
-      } else if (newestGrid[2] == 5) {
-        newestGrid[3] = 11;
-      }
-      console.log("eerste ring links");
-    } else if (newestGrid[2] == (gridCorners[1])) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - 2)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 3)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 4)][3];
-      console.log("eerste links");
-    } else if (newestGrid[2] < (gridCorners[2] - 2)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 3)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 4)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
-      console.log("midden links");
-    } else if (newestGrid[2] == (gridCorners[2] - 2)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 4)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
-      console.log("een na laatste links");
-    } else if (newestGrid[2] == (gridCorners[2] - 1)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
-      console.log("laastste links");
-    }
-  } else if (newestGrid[2] < gridCorners[3]) {
-    if (newestGrid[1] == 1) {
-      if (newestGrid[2] == 6) {
-        newestGrid[3] = 23;
-      } else if (newestGrid[2] == 7) {
-        newestGrid[3] = 25;
-      }
-      console.log("eerste ring onder");
-    } else if (newestGrid[2] == (gridCorners[2])) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - 2)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 6)][3];
-      console.log("eerste onder");
-    } else if (newestGrid[2] < (gridCorners[3] - 2)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 6)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 7)][3];
-      console.log("midden onder");
-    } else if (newestGrid[2] == (gridCorners[3] - 2)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 5)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 6)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 7)][3];
-      console.log("een na laatste onder");
-    } else if (newestGrid[2] == (gridCorners[3] - 1)) {
-      newestGrid[3] = grid[(newestGrid[0] - 1)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 6)][3];
-      newestGrid[3] += grid[(newestGrid[0] - newestGrid[2] - tilesInCircle((newestGrid[1] - 1)) + (newestGrid[2] - 1) - 7)][3];
-      console.log("laastste onder");
     }
   }
-  grid[grid.length] = newestGrid;
-  //console.log("grid return whole is: " + grid);
-  return grid;
-}
-
-function calcAll() {
-  var gridIni = gridNul();
-  var grid = gridIni[0];
-  var gridNamen = gridIni[1];
-  var puzzleInput = 312051;
-  var gridValue = 0;
-  var counter = 0
-  while (gridValue < puzzleInput && counter < 400) {
-    counter ++;
-    grid = calcNext(grid);
-    console.log("grid return whole is: ");
-    console.log(grid);
-    gridValue = grid[grid.length - 1][3];
-    console.log("gridValue in loop is: " + gridValue);
-  }
+  return [weight, 0, name];
 }
