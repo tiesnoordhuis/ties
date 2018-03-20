7b0317fb23bd1d1a75b5310629e6148269c826a2
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-03-20 15:11:55 +0100

    puzzle day 7 af!!!

--- advent2.js
+++ advent2.js
@@ -43,7 +43,7 @@ function doAll(x) {
   }
   var refCounter = 0;
   var answerArray = [];
-  while (refCounter < 3) {
+  while (refCounter < 20) {
     for (var i = 0; i < list.length; i++) {
       if (list[i][3] == refCounter) {
         for (var k = 0; k < list[i][1].length; k++) {
@@ -52,7 +52,7 @@ function doAll(x) {
           for (var j = 0; j < list[i][5].length; j++) {
             if (list[i][5][j][1] == list[i][1][k]) {
               alreadySet = 1;
-              console.log("already set");
+              //console.log("already set");
             }
           }
           if (answerArray[0][1] == 1 && alreadySet == 0) {
@@ -62,14 +62,33 @@ function doAll(x) {
         //console.log(list[i][5].length);
         //console.log(list[i][1].length);
         if (list[i][5].length == list[i][1].length || list[i][3] == 0) {
-          console.log("hetzelfde");
+          //console.log("hetzelfde");
           //list[i][3] = refCounter; //niet nodig
-          console.log(list[i][3]);
+          //console.log(list[i][3]);
         } else if (list[i][5].length != list[i][1].length) {
           list[i][3] = (refCounter + 1);
-          console.log("niet hetzelfde");
+          //console.log("niet hetzelfde");
         }
-        console.log(list[i]);
+        //console.log(list[i]);
+      }
+    }
+    for (var i = 0; i < list.length; i++) {
+      if (list[i][3] == refCounter) {
+        var totalWeight = 0;
+        var weightCompare = []
+        for (var k = 0; k < list[i][5].length; k++) {
+          totalWeight += list[i][5][k][0];
+          weightCompare[k] = list[i][5][k][0];
+        }
+        for (var j = 1; j < weightCompare.length; j++) {
+          if (weightCompare[0] != weightCompare[j]) {
+            console.log("weight fout in");
+            console.log(list[i]);
+          }
+        }
+        //console.log(totalWeight);
+        list[i][4] = list[i][2] + totalWeight;
+        //console.log(list[i]);
       }
     }
     //console.log(refCounter);
@@ -82,7 +101,7 @@ function lookupWeight(name, cycle, checkList) {
   for (var i = 0; i < checkList.length; i++) {
     if (checkList[i][0] == name) {
       if (checkList[i][3] < cycle) {
-        weight = checkList[i][2];
+        weight = checkList[i][4];
         return [weight, 1, name];
       }
     }
