b9aa1d74bf4f2bb374946ecfac281eac1c8ffc28
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-03-20 23:33:10 +0100

    day 8 begin

--- advent.js
+++ advent.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-var input = fs.readFile("input5.txt", "utf8", (err, data) => {
+var input = fs.readFile("input6.txt", "utf8", (err, data) => {
   if (err) {
     return console.error(err);
   }
@@ -14,123 +14,18 @@ var input = fs.readFile("input5.txt", "utf8", (err, data) => {
 var x;
 
 function doAll(x) {
-  var list2 = [];
   var list = x.split("\n");
   list.pop();
   for (var i = 0; i < list.length; i++) {
-    var newListEntry = list[i].split(" -> ");
-    //newListEntry[0] = newListEntry[0].split(" ")[0];
-    list[i] = newListEntry;
-    if (list[i].length > 1) {
-      var newListEntrySecond = list[i][1].split(", ");
-      list[i][1] = newListEntrySecond;
-    } else {
-      list[i][1] = [""];
-    }
-    var newListEntryWeight = list[i][0].split(" (");
-    list[i][0] = newListEntryWeight[0];
-    list[i][2] = Number(newListEntryWeight[1].split(")")[0]);
-    list[i][3] = 0;
-    list[i][4] = Number(list[i][2]);
-    list2[i] = [i, list[i][1].slice()];
-    //console.log(list[i]);
-  }
-  var listWeights = [];
-  for (var i = 0; i < list.length; i++) {
-    listWeights[i] = [];
-    for (var j = 0; j < list[i][1].length; j++) {
-      for (var k = 0; k < list.length; k++) {
-        if (list[i][1][j] == list[k][0] && k != i) {
-          listWeights[i][j] = list[k][2];
-        }
-      }
-    }
-  }
-  //console.log(listWeights);
-  var falseWeights = [];
-  for (var i = 0; i < listWeights.length; i++) {
-    var checked = 0;
-    var referenceValue = listWeights[i][0];
-    for (var j = 1; j < listWeights[i].length; j++) {
-      if (listWeights[i][j] == referenceValue) {
-
-      } else if (listWeights[i][j] > referenceValue) {
-        checked = 1;
-      } else if (listWeights[i][j] < referenceValue) {
-        checked = 1;
-      }
-    }
-    if (checked != 0) {
-      //console.log(listWeights[i]);
-      falseWeights.push([i, listWeights[i]])
-    }
-  }
-  //console.log(falseWeights);
-  var whileCheck = 0
-  var whileCounter = 1;
-  //console.log(checkDone(list));
-  while (whileCheck == 0) {
-    if (checkDone(list)) {
-      whileCheck = 1;
-    }
-    for (var i = 0; i < list.length; i++) {
-      if (list[i][1] == "") {
-        if (list[i][3] == 0) {
-            list[i][3] = whileCounter;
-        }
-         //console.log(list[i]);
-      } else {
-        //console.log(list[i]);
-        for (var k = 0; k < list.length; k++) {
-          for (var j = 0; j < list[i][1].length; j++) {
-            if (list[k][0] == list[i][1][j] && list[k][3] >= 1) {
-              //console.log(list[i]);
-              list[i][1].splice(j, 1);
-              list[i][2] += list[k][2];
-              //console.log(list[i]);
-            }
-          }
-        }
-      }
-      //console.log(list[i]);
-    }
-    whileCounter ++;
+    list[i] = list[i].split(" ");
+    list[i].unshift(i, 0);
+    list[i][4] = Number(list[i][4]);
+    list[i][8] = Number(list[i][8]);
+    console.log(list[i]);
   }
   for (var i = 0; i < list.length; i++) {
-    //console.log(list[i]);
-    //console.log(list2[i][1]);
-  }
-  var weightList = [];
-  for (var i = 0; i < list.length; i++) {
-    if (list2[i][1] != "") {
-      var checkSame = 0;
-      for (var j = 0; j < list.length; j++) {
-        for (var k = 0; k < list2[i].length; k++) {
-          if (list2[i][1][k] == list[j][0]) {
-            checkSame = list[j][4];
-            weightList.push([i, list[i], list2[i], list[j], checkSame]);
-            //console.log(weightList[(weightList.length - 1)]);
-            //console.log(list[i]);
-            //console.log(list[j]);
-          }
-        }
-      }
+    if (checkCondition(list[i]), list) {
+      var newValue = doProcess(list[i], list);
     }
   }
-  for (var i = 0; i < weightList.length; i++) {
-    console.log(weightList[i]);
-  }
-}
-
-function checkDone(list) {
-  var checkCounter = 0;
-  for (var i = 0; i < list.length; i++) {
-    if (list[i][3] < 1) {
-      checkCounter ++;
-      if (checkCounter > 2) {
-        return false;
-      }
-    }
-  }
-  return true;
 }
