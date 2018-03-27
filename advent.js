2de2efcd541070b213bfa876b361d63d0e811614
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-03-27 13:15:45 +0200

    day 8 finished

--- advent.js
+++ advent.js
@@ -23,13 +23,107 @@ function doAll(x) {
     list[i][8] = Number(list[i][8]);
     console.log(list[i]);
   }
+  var highest = 0;
+  var listRegister = [];
   for (var i = 0; i < list.length; i++) {
-    if (checkCondition(list[i], list)) {
-      var newValue = doProcess(list[i], list);
+    var registerFound = 0;
+    for (var j = 0; j < listRegister.length; j++) {
+      if (list[i][2] == listRegister[j][2]) {
+        registerFound = 1;
+        break;
+      }
+    }
+    if (registerFound == 0) {
+      listRegister.push(list[i].slice(0, 3));
+      console.log(listRegister[(listRegister.length - 1)]);
     }
   }
+  for (var i = 0; i < list.length; i++) {
+    if (checkCondition(list[i], listRegister)) {
+      listRegister = doProcess(list[i], listRegister);
+      highest = checkHighest(listRegister, highest)
+    }
+  }
+  for (var i = 0; i < listRegister.length; i++) {;
+    console.log(listRegister[i]);
+  }
+  console.log(highest);
+}
+
+function checkHighest(checkList, highValue) {
+  for (var i = 0; i < checkList.length; i++) {
+    if (checkList[i][1] > highValue) {
+      highValue = checkList[i][1];
+      console.log(highValue);
+    }
+  }
+  return highValue;
 }
 
 function checkCondition(element, checkList) {
-  
+  for (var i = 0; i < checkList.length; i++) {
+    if (checkList[i][2] == element[6]) {
+      if (checkCompare(checkList[i][1], element[7], element[8]) == 1) {
+        console.log(element + "  is waar voor:");
+        console.log(checkList[i]);
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function checkCompare(value1, comparator, value2) {
+  if (comparator == ">") {
+    if (value1 > value2) {
+      console.log(value1 + comparator + value2);
+      return 1;
+    }
+  }
+  if (comparator == ">=") {
+    if (value1 >= value2) {
+      console.log(value1 + comparator + value2);
+      return 1;
+    }
+  }
+  if (comparator == "==") {
+    if (value1 == value2) {
+      console.log(value1 + comparator + value2);
+      return 1;
+    }
+  }
+  if (comparator == "<=") {
+    if (value1 <= value2) {
+      console.log(value1 + comparator + value2);
+      return 1;
+    }
+  }
+  if (comparator == "<") {
+    if (value1 < value2) {
+      console.log(value1 + comparator + value2);
+      return 1;
+    }
+  }
+  if (comparator == "!=") {
+    if (value1 != value2) {
+      console.log(value1 + comparator + value2);
+      return 1;
+    }
+  }
+  return 0;
+}
+
+function doProcess(element, checkList) {
+  for (var i = 0; i < checkList.length; i++) {
+    if (element[2] == checkList[i][2]) {
+      if (element[3] == "inc") {
+        checkList[i][1] += element[4];
+        break;
+      } else if (element[3] == "dec") {
+        checkList[i][1] -= element[4];
+        break;
+      }
+    }
+  }
+  return checkList;
 }
