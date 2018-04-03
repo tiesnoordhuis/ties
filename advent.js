42c2e63909ec5fe5694998b38c358f313d1d5211
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-04-03 18:29:29 +0200

    day 10 start

--- advent.js
+++ advent.js
@@ -1,113 +1,101 @@
 const fs = require("fs");
 
-var input = fs.readFile("input7.txt", "utf8", (err, data) => {
+var input = fs.readFile("input8.txt", "utf8", (err, data) => {
   if (err) {
     return console.error(err);
   }
   else {
     x = data;
     doAll(x);
-    return x;
     }
 });
 
-var x;
-
 function doAll(x) {
-  var list = x;
-  console.log(list);
-  var list2 = [];
-  var calcString = "";
-  var garbageString = "";
-  var garbageN = 0;
-  var currentLevel = 0;
-  var inGarbage = false;
-  var endGarbage = false;
-  var isIgnored = false;
-  var stepResponse = [];
-  var scoreResponse = [];
-  var score = 0;
-  var level = 0;
+  var input = x.split(",");
+  var lengths = [];
+  var currentPosition = 0;
+  var skipSize = 0;
+  for (var i = 0; i < input.length; i++) {
+	  lengths[i] = Number(input[i]);
+  }
+  console.log(lengths);
+  var list = [];
+  list.length = 256;
   for (var i = 0; i < list.length; i++) {
-    list2.push(list[i]);
+	  list[i] = i;
   }
-  for (var i = 0; i < (list2.length - 1); i++) {
-    stepResponse = doStep(list2[i], i, inGarbage, isIgnored, garbageN);
-    inGarbage = stepResponse[0];
-    endGarbage = stepResponse[1];
-    isIgnored = stepResponse[2];
-	garbageN = stepResponse[3];
-    if (inGarbage == false && endGarbage == false) {
-      calcString += list2[i];
-    } else {
-		if (isIgnored == false && stepResponse.length == 4) {
-			garbageString += list2[i];
-		}
+  for (var i = 0; i < lengths.length; i++) {
+	  console.log("currentPosition");
+	  console.log(currentPosition);
+	  var selectedArray = selectArray(list, currentPosition, lengths[i]);
+	  var flipedArray = reverseArray(selectedArray.slice());
+	  console.log("selectedArray");
+	  console.log(selectedArray[0] + " " + selectedArray[selectedArray.length - 1]);
+	  console.log("flipedArray");
+	  console.log(flipedArray[0] + " " + flipedArray[flipedArray.length - 1]);
+	  list = insertArrayBackIn(list, flipedArray, currentPosition);
+	  currentPosition = skipToNextPosition(currentPosition, skipSize, lengths[i]);
+	  skipSize ++;
+	  viewList(list);
+  }  
+}
+
+function viewList(list) {
+	console.log("");
+	for (var i = 0; i < list.length; i++) {
+	  console.log(i + "  " + list[i]);
+  }
+}
+
+function insertArrayBackIn(list, flipedArray, currentPosition) {
+	var listT = list.slice();
+	if ((currentPosition + flipedArray.length) > 255) {
+		var flipedArrayP1 = [];
+		var flipedArrayP2 = [];
+		flipedArrayP1 = flipedArray.slice(0, (255 - currentPosition));
+		flipedArrayP2 = flipedArray.slice((255 - currentPosition));
+		listT.splice(currentPosition);
+		listT = listT.concat(flipedArrayP1);
+		listT.splice(0, flipedArrayP2.length);
+		listT = flipedArrayP2.concat(listT);
+		} else {
+			var endOfList = listT.slice((currentPosition + flipedArray.length));
+			listT.splice(currentPosition);
+			listT = listT.concat(flipedArray);
+			listT = listT.concat(endOfList);
 	}
-  }
-  console.log(calcString);
-  console.log(garbageString);
-  for (var i = 0; i < calcString.length; i++) {
-    scoreResponse = calcScore(calcString[i], level, score);
-    score = scoreResponse[0];
-    level = scoreResponse[1];
-  }
-  console.log(score);
-  var scoreGarbage = countGarbage(garbageString);
-  console.log(scoreGarbage);
-  console.log(garbageN);
-  var scoreGarbage = scoreGarbage - (2 * garbageN);
-  console.log(scoreGarbage);
+	return listT;
 }
 
-function countGarbage(garbageString) {
-	var scoreGarbage = 0;
-	for (var i = 0; i < garbageString.length; i++) {
-		scoreGarbage ++;
+function skipToNextPosition(currentPosition, skipSize, length) {
+	var returnPosition = 0;
+	returnPosition = currentPosition + skipSize + length;
+	if (returnPosition > 255) {
+		returnPosition -= 256;
 	}
-	return scoreGarbage;
+	return returnPosition;
 }
 
-function calcScore(currentChar, level, score) {
-	var returnArray = [score, level];
-  if (currentChar == "{") {
-	level ++;
-	score += level;
-  } else if (currentChar == "}") {
-	level --;
-  } else if (currentChar == ",") {
-
-  } else {
-    console.log("ERROR");
-	console.log(currentChar);
-  }
-  returnArray = [score, level];
-  return returnArray;
+function selectArray(list, currentPosition, stepLength) {
+	var returnArray = [];
+	var returnArrayP1 = [];
+	var returnArrayP2 = [];
+	var overflow = 0;
+	if ((currentPosition + stepLength) > list.length) {
+		returnArrayP1 = list.slice(currentPosition, (list.length - 1));
+		overflow = stepLength - returnArrayP1.length;
+		returnArrayP2 = list.slice(0, overflow);
+		returnArray = returnArrayP1.concat(returnArrayP2);
+	} else {
+		returnArray = list.slice(currentPosition, (stepLength + currentPosition));
+	}
+	return returnArray;
 }
 
-function doStep(currentChar, i, inGarbage, isIgnored, garbageN) {
-  var returnArray = [false, false, false, garbageN];
-  var ignoreArray = [true, false, false, garbageN, true];
-  if (inGarbage == false) {
-    if (currentChar == "<") {
-      returnArray[0] = true;
-    } else {
-      returnArray[0] = false;
-    }
-  } else if (inGarbage == true) {
-    if (isIgnored == true) {
-      return ignoreArray;
-    } else if (currentChar == "!") {
-      returnArray[0] = true;
-      returnArray[2] = true;
-    } else if (currentChar == ">") {
-      returnArray[0] = false;
-      returnArray[1] = true;
-	  garbageN ++;
-	  returnArray[3] = garbageN;
-    } else {
-      returnArray[0] = true;
-    }
-  }
-  return returnArray;
+function reverseArray(array) {
+	var arrayReference = array.slice();
+	for (var i = 0; i < array.length; i++) {
+		array[i] = arrayReference[((array.length - 1) - i)];
+	}
+	return array;
 }
