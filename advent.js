e9b820defbd736f950acb820df10013f9948fcb8
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-06-19 11:49:57 +0200

    day 16 half

--- advent.js
+++ advent.js
@@ -26,19 +26,46 @@ function doAll(x) {
   }
   for (var i = 0; i < dancemoves.length; i++) {
     switch (dancemoves[i][0]) {
-      case "s":
+      case "s": {
         console.log("doe spin");
+        console.log(dancers.join());
+        let n = dancemoves[i][1];
+        console.log(" spin doen met " + n);
+        let end = dancers.slice(-n);
+        for (var j = 0; j < n; j++) {
+          dancers.pop();
+        }
+        dancers = end.concat(dancers);
         break;
-      case "x":
+      }
+      case "x": {
         console.log("doe exchange");
+        console.log(dancers.join());
+        var n = dancemoves[i][1].split("/");
+        n[0] = Number(n[0]);
+        n[1] = Number(n[1]);
+        console.log("exchange met values " + n);
+        if (n[0] < n[1]) {
+          let temp = dancers[n[0]];
+          dancers.splice(n[0], 1, dancers[n[1]]);
+          dancers.splice(n[1], 1, temp)
+        } else if (n[1] < n[0]) {
+          let temp2 = dancers[n[0]];
+          dancers.splice(n[0], 1, dancers[n[1]]);
+          dancers.splice(n[1], 1, temp2);
+        }
         break;
-      case "p":
+      }
+      case "p": {
         console.log("doe partner");
+        console.log(dancers.join());
         break;
-      default:
+      }
+      default: {
         console.log("unkown case");
         console.log("op positie: " + i);
         console.log("met instructie: " + dancemoves[i]);
+      }
     }
   }
 }
