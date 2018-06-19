859ee458959bee2a27bd3a5383a89f63b6c6035c
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-06-19 16:12:24 +0200

    day 16 part 1 complete part 2 first version

--- advent.js
+++ advent.js
@@ -17,55 +17,63 @@ function doAll(x) {
   }
   var dancemoves = [];
   var input = x.split(",");
-  input.pop();
+  //input.pop();
   for (var i = 0; i < input.length; i++) {
     dancemoves[i] = [];
     dancemoves[i][0] = input[i].slice(0, 1);
     dancemoves[i][1] = input[i].slice(1);
     //console.log(dancemoves[i]);
   }
-  for (var i = 0; i < dancemoves.length; i++) {
-    switch (dancemoves[i][0]) {
-      case "s": {
-        console.log("doe spin");
-        console.log(dancers.join());
-        let n = dancemoves[i][1];
-        console.log(" spin doen met " + n);
-        let end = dancers.slice(-n);
-        for (var j = 0; j < n; j++) {
-          dancers.pop();
+  for (var l = 0; l < 1000000000; l++) {
+    for (var i = 0; i < dancemoves.length; i++) {
+      switch (dancemoves[i][0]) {
+        case "s": {
+          //console.log("doe spin");
+          //console.log(dancers.join());
+          let n = dancemoves[i][1];
+          //console.log(" spin doen met " + n);
+          let end = dancers.slice(-n);
+          for (var j = 0; j < n; j++) {
+            dancers.pop();
+          }
+          dancers = end.concat(dancers);
+          break;
         }
-        dancers = end.concat(dancers);
-        break;
-      }
-      case "x": {
-        console.log("doe exchange");
-        console.log(dancers.join());
-        var n = dancemoves[i][1].split("/");
-        n[0] = Number(n[0]);
-        n[1] = Number(n[1]);
-        console.log("exchange met values " + n);
-        if (n[0] < n[1]) {
+        case "x": {
+          //console.log("doe exchange");
+          //console.log(dancers.join());
+          let n = dancemoves[i][1].split("/");
+          n[0] = Number(n[0]);
+          n[1] = Number(n[1]);
+          //console.log("exchange met values " + n);
           let temp = dancers[n[0]];
           dancers.splice(n[0], 1, dancers[n[1]]);
-          dancers.splice(n[1], 1, temp)
-        } else if (n[1] < n[0]) {
-          let temp2 = dancers[n[0]];
-          dancers.splice(n[0], 1, dancers[n[1]]);
-          dancers.splice(n[1], 1, temp2);
+          dancers.splice(n[1], 1, temp);
+          break;
         }
-        break;
-      }
-      case "p": {
-        console.log("doe partner");
-        console.log(dancers.join());
-        break;
-      }
-      default: {
-        console.log("unkown case");
-        console.log("op positie: " + i);
-        console.log("met instructie: " + dancemoves[i]);
+        case "p": {
+          //console.log("doe partner");
+          //console.log(dancers.join());
+          let n = dancemoves[i][1].split("/");
+          //console.log("met  parteners " + n);
+          n[0] = dancers.findIndex((dancer) => {
+            return n[0] == dancer;
+          });
+          n[1] = dancers.findIndex((dancer) => {
+            return n[1] == dancer;
+          });
+          let temp = dancers[n[0]];
+          dancers.splice(n[0], 1, dancers[n[1]]);
+          dancers.splice(n[1], 1, temp);
+          break;
+        }
+        default: {
+          console.log("unkown case");
+          console.log("op positie: " + i);
+          console.log("met instructie: " + dancemoves[i]);
+        }
       }
     }
   }
+  console.log(dancers.join(""));
 }
