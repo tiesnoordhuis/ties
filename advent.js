0468caa3e9901feb120533ecd23e38022ed97000
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-06-21 16:13:22 +0200

    day 16 part 2 first try

--- advent.js
+++ advent.js
@@ -1,14 +1,41 @@
-function doAll() {
-  var buffer = [0];
-  var currentPos = 0;
-  for (var step = 0; step < 2018; step++) {
-    console.log(step);
-    buffer = addStepToBuffer(step, currentPos, buffer);
+function doAll(loop) {
+  var buffer = [];
+  buffer[0] = [];
+  buffer[1] = 0;
+  for (var step = 0; step < 50000000; step++) {
+    console.log("step " + step);
+    buffer = addStepToBuffer(step, buffer, loop);
+    //console.log("new position " + buffer[1]);
+  }
+  var valueAfter2017 = findStep1Value(buffer);
+  console.log(valueAfter2017);
+}
+
+function findStep1Value(buffer) {
+  var temp = buffer[0].findIndex((element) => {
+    return element === 0;
+  });
+  return buffer[0][(temp + 1)];
+}
+
+function addStepToBuffer(step, buffer, loop) {
+  buffer[1] = calcNewPos(buffer[1], step, loop);
+  buffer[0].splice((buffer[1] + 1), 0, step);
+  buffer[1] ++;
+  return buffer;
+}
+
+function calcNewPos(currentPos, step, loop) {
+  if (step === 0) {
+    return 0;
+  } else {
+    var temp = loop % step;
+    temp += currentPos;
+    if (temp > step) {
+      temp -= step;
+    }
+    return temp;
   }
 }
 
-function addStepToBuffer(step, currentPos, buffer) {
-
-}
-
-doAll();
+doAll(355);
