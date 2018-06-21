cdab922294d6ddc5b565305c307477efa3ca6c84
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-06-21 10:57:24 +0200

    day 16 complete

--- advent2.js
+++ advent2.js
@@ -10,27 +10,56 @@ var input = fs.readFile("input12.txt", "utf8", (err, data) => {
     }
 });
 
-var compact = [];
-
 function doAll(x) {
   var dancers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"];
   console.log(dancers.join(""));
   var input = x.split(",");
   var dancemoves = makeDancemoves(input);
-  var dancersRef = []
-  for (var cycle = 0; cycle < array.length; cycle++) {
-    dancersRef[cycle] = dancers.slice();
+  var dancersRef = [];
+  for (var cycle = 0; cycle < 1000; cycle++) {
+    dancersRef.push(dancers.slice().join(""));
+    console.log(cycle);
+    //console.log(dancersRef);
     for (var i = 0; i < dancemoves.length; i++) {
       dancers = doDancemove(dancers, dancemoves[i]);
-      console.log(dancers.join(""));
-      console.log(compact);
+      //console.log(dancers.join(""));
     }
-    console.log(dancers.join(""));
+    //console.log(dancers.join(""));
+    var foundCycle = checkRef(cycle, dancersRef);
+    console.log(foundCycle);
+    if (foundCycle[0]) {
+      calcCycles(foundCycle[1], dancersRef)
+      break;
+    }
+  }
+}
+
+function calcCycles(completeCycle, dancersRef) {
+  var leftover = 1000000000 % completeCycle;
+  console.log(leftover);
+  console.log(dancersRef[leftover]);
+}
+
+function checkRef(cycle, dancersRef) {
+  var latest = dancersRef[(dancersRef.length - 1)];
+  var index = dancersRef.findIndex((element) => {
+    //console.log(element);
+    return element == latest;
+  });
+  if (index === cycle) {
+    return [false];
+  } else if (index >= 0) {
+    console.log(index);
+    console.log(dancersRef[index]);
+    console.log(cycle);
+    console.log(dancersRef[cycle]);
+    return [true, cycle];
+  } else {
+    return [false];
   }
 }
 
 function doDancemove(dancers, dancemovesSingle) {
-  compact[0] = [true];
   if (dancemovesSingle[0] === "s") {
     let n = dancemovesSingle[1];
     //console.log(" spin doen met " + n);
