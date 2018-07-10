e4a7d5515805500bcaa5c37479bdb38d61d82c7e
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-07-10 23:00:43 +0200

    inputs done

--- advent.js
+++ advent.js
@@ -11,13 +11,13 @@ var input = fs.readFile("input16.txt", "utf8", (err, data) => {
 });
 
 function doAll(x) {
-  var input = x.split("\n");
+  var input = x.split("\r\n");
   input.pop();
-  console.log(input);
+  //console.log(input);
   const rules2 = buildRules2Object(input);
   const rules3 = buildRules3Object(input);
-  console.log(rules2);
-  //console.log(rules3);
+  console.log(rules3);
+  console.log(rules3[15].input);
 }
 
 function buildRules2Object(input) {
@@ -75,8 +75,50 @@ function buildRules3Object(input) {
   for (var i = 0; i < rulesConstruct.length; i++) {
     temp = rulesConstruct[i].split(" => ");
     temp[0] = temp[0].split("/");
+    for (var j = 0; j < temp[0].length; j++) {
+      temp[0][j] = temp[0][j].split("");
+    }
     temp[1] = temp[1].split("/");
+    for (var j = 0; j < temp[1].length; j++) {
+      temp[1][j] = temp[1][j].split("");
+    }
     returnArray[i] = { rule: i, input: temp[0], output: temp[1]};
+    returnArray[i].input = extendInputs3(returnArray[i].input);
   }
   return returnArray;
+}
+
+function extendInputs3(singleInput) {
+  var rotatedInputs3 = rotateInputs3(singleInput);
+  var flippedInputs3 = flipInputs3(singleInput);
+  var returnArray = [singleInput, rotatedInputs3[0], rotatedInputs3[1], rotatedInputs3[2], flippedInputs3[0], flippedInputs3[1]];
+  return returnArray;
+}
+
+function rotateInputs3(singleInput) {
+  var rotate1 = [];
+  rotate1[0] = [singleInput[2][0], singleInput[1][0], singleInput[0][0]];
+  rotate1[1] = [singleInput[2][1], singleInput[1][1], singleInput[0][1]];
+  rotate1[2] = [singleInput[2][2], singleInput[1][2], singleInput[0][2]];
+  var rotate2 = [];
+  rotate2[0] = [rotate1[2][0], rotate1[1][0], rotate1[0][0]];
+  rotate2[1] = [rotate1[2][1], rotate1[1][1], rotate1[0][1]];
+  rotate2[2] = [rotate1[2][2], rotate1[1][2], rotate1[0][2]]
+  var rotate3 = [];
+  rotate3[0] = [rotate2[2][0], rotate2[1][0], rotate2[0][0]];
+  rotate3[1] = [rotate2[2][1], rotate2[1][1], rotate2[0][1]];
+  rotate3[2] = [rotate2[2][2], rotate2[1][2], rotate2[0][2]]
+  return [rotate1, rotate2, rotate3];
+}
+
+function flipInputs3(singleInput) {
+  var flipHori = [];
+  flipHori[0] = singleInput[2];
+  flipHori[1] = singleInput[1];
+  flipHori[2] = singleInput[0];
+  var flipVerti = [];
+  flipVerti[0] = [singleInput[0][2], singleInput[0][1], singleInput[0][0]];
+  flipVerti[1] = [singleInput[1][2], singleInput[1][1], singleInput[1][0]];
+  flipVerti[2] = [singleInput[2][2], singleInput[2][1], singleInput[2][0]];
+  return [flipHori, flipVerti];
 }
